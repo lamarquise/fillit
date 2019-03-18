a46b23118bc695cce93578ed7792194351ea0d1d
Eric LAZO <erlazo@e3r9p17.42.fr>
2019-03-18 17:22:15 +0100

    some progress on the parser

--- src/main.c
+++ src/main.c
@@ -6,21 +6,65 @@
 /*   By: erlazo <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/17 17:49:19 by erlazo            #+#    #+#             */
-/*   Updated: 2019/03/17 18:39:22 by erlazo           ###   ########.fr       */
+/*   Updated: 2019/03/18 17:19:50 by erlazo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fillit.h"
 
-int		ret()
+int		ret()								// how does exit work. is it what we want here? 
 {
 	write(1, "error\n", 6);
 	return (0);
 }
 
 
-int		read_file()								// would it actually be more efficient to have a massive read that is larger than the size of the largest possible valid file?
+int		read_file(int fd)			// this thing reads one piece at a time and returns a val coresponding to that piece
+{
+	int		i;
+	int		total;
+	int		change;
+	int		ch;
+	int		start;				// could put these in a table.
+	int		end;
+	char	buff[2];
 
+	ch = 0;
+	start = 5;
+	end = 5;
+	number = 0;
+	buff[1] = '\0';
+	while (ch < 6)								//
+	{
+		i = 0;
+		change = 0;
+		while (ch < 5 && i < 5 && read(fd, buff, 1) > 0)		// if at any point buff not == . # or \n then error
+		{
+			if (!(buff[0] == '.' || buff[0] == '#' || buff[0] == '\n'))
+				return(ret());
+			if (i == 4 && buff[0] != '\n')
+				return (ret());
+			if (buff[0] == '#')					// how do i use this info on the next line to come to conclusions.
+			{
+				++change;
+				if (start == 5)
+					start = i;
+				if (end == 5 || end == i - 1)
+					end = i;
+				else
+					return (ret());
+			}
+			total += change; 
+			++i;
+		}
+		if (ch == 5 && !(read(fd, buff, 1) == 0 && buff != '\n'))		// close enough, basically, if it reads it had better be \n.	will definitly need help...
+			return (ret());
+		++ch;
+		if ( //something.... ch = 0 or -1 or whatever
+	}
+	
+
+}
 
 
 int		main(int ac, char **av)
